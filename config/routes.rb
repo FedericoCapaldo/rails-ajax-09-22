41b235d8dfbfc9c53247b2de9811a64d71c4f2a6
Federico Capaldo <federico.capaldo1@gmail.com>
2022-09-02 02:22:19 +0100

    AJAX EXAMPLE PAGES added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+
+  get 'xhr-example', to: 'pages#xhr_example'
+  get 'fetch-example', to: 'pages#fetch_example'
+  get 'fetch-example-modern', to: 'pages#fetch_example_modern'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
